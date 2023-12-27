00e8e4c51cd5a6accf6f61f5a33dd97cab24b68d
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-12-27 16:53:40 +0700

    Add InsertViewModel into PenyediaViewModel

--- app/src/main/java/com/example/project8_classc/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project8_classc/ui/PenyediaViewModel.kt
@@ -6,11 +6,16 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.project8_classc.KontakApplication
 import com.example.project8_classc.ui.home.viewmodel.HomeViewModel
+import com.example.project8_classc.ui.kontak.viewmodel.InsertViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepository)
         }
     }
 }
